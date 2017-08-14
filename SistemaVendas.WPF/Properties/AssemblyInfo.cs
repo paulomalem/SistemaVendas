30a987781c4634a660dfbedc96c12c0997b52378
Paulo Alem <paulomalem@gmail.com>
2017-08-14 15:38:35 -0300

    Adicionando Testes em Controller e DAO

--- SistemaVendas.WPF/Properties/AssemblyInfo.cs
+++ SistemaVendas.WPF/Properties/AssemblyInfo.cs
@@ -53,3 +53,9 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("SistemaVendas.WPF.Tests")]
+[assembly: InternalsVisibleTo("SistemaVendas.WPF.Explorables")]
+[assembly: InternalsVisibleTo("SistemaVendas.WPF.DAO.Tests")]
+[assembly: InternalsVisibleTo("SistemaVendas.WPF.Controllers.Tests")]
+[assembly: InternalsVisibleTo("SistemaVendas.WPF.Controller.Tests")]
+
